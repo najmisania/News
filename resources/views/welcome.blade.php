dbbcb49ee7a70109713ec3a2de2ff101e234a912
Sri Rahayu <07,srirahayu@gmail.com>
2019-10-18 01:05:51 +0700

    PALING BARU BANGET

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @include('navbar.head')
     <body>
-        <header>
+        <header>         
 
             <div class="header-top">
                 <div class="container">
@@ -11,7 +11,7 @@
                                 <h1 style="color: white;">DTS News</h1>
                             </a>
                         </div>
-                        </div>
+                      </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6 header-top-right no-padding">
                             <ul>
                                 <li><a href="{{route('login')}}"><span class="lnr lnr-phone-user"></span><span>Login</span></a></li>
