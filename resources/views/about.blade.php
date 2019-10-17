7054f0e83b8f83389495ece2becb56802aac573f
Najmi Sania <najmisania@gmail.com>
2019-10-17 11:25:46 +0700

    tampilan ubah about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>News Digital Talent Sholarship</title>
+  <title>News Digital Talent</title>
 
   <!-- Bootstrap core CSS -->
   <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
