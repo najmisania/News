32ed1399020e6307d0e275a6676dc49be19a7a5d
Sri Rahayu <07,srirahayu@gmail.com>
2019-10-18 16:14:47 +0700

    paling baru

--- resources/views/authAdmin/login.blade.php
+++ resources/views/authAdmin/login.blade.php
@@ -1,6 +1,23 @@
-@extends('layouts.app')
+@extends('navbar.head')
+<div class="header-top">
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-6 header-top-right no-padding">
+                <div class="col-lg-4 col-md-4 col-sm-12 logo-left no-padding">
+                <a href="/">
+                    <h1 style="color: white;">DTS News</h1>
+                </a>
+            </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-6 header-top-right no-padding">
+                <ul>
+                    <li><a href="{{route('login')}}"><span class="lnr lnr-envelope"></span><span>Login</span></a></li>
+                </ul>
+            </div>
 
-@section('content')
+        </div>
+    </div>
+</div> <br><br><br>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +87,6 @@
         </div>
     </div>
 </div>
-@endsection
+<br><br><br>
+
+@extends('navbar.footer')
