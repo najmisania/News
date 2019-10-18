2f1acf2e9333b116e82c6dcf8fc8bfeb7bd5fc27
AldhyMahardika <aldhymahardika@gmail.com>
2019-10-18 15:49:19 +0700

    pembuatan login untuk admin dan user

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/about', 'About@buka_about' );
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function(){
+	Route::get('/login', 'AuthAdmin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('/login', 'AuthAdmin\LoginController@login')->name('admin.login.submit');
+	Route::get('/', 'AdminController@index')->name('admin.home');
+});
