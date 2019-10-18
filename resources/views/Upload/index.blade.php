a1a912f1f5ded00abda76a87c87ef7cb41dccd7a
Najmi Sania <najmisania@gmail.com>
2019-10-18 23:13:05 +0700

    tambah tampilan upload

--- resources/views/Upload/index.blade.php
+++ resources/views/Upload/index.blade.php
@@ -1,97 +1,82 @@
 @include('navbar.head')
-                <div class="header-top">
-                <div class="container">
-                    <div class="row justify-content-between align-items-center">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-6 header-top-right no-padding">
-                            <div class="col-lg-4 col-md-4 col-sm-12 logo-left no-padding">
-                            <a href="/">
-                                <h1 style="color: white;">DTS News</h1>
-                            </a>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-            </div>
+<div class="header-top">
+	<div class="container">
+		<div class="row justify-content-between align-items-center">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-6 header-top-right no-padding">
+				<div class="col-lg-4 col-md-4 col-sm-12 logo-left no-padding">
+					<a href="/">
+						<h1 style="color: white;">DTS News</h1>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <div class="container">
 
-    <div class="card o-hidden border-0 shadow-lg my-15 col-lg-12 mx-auto">
-      <div class="card-body p-0">
-        <!-- Nested Row within Card Body -->
-        <div class="card">
-
-          <div class="col-lg-12">
-            <div class="p-5">
-              <div class="text-center section_title">
-                <h3 class="text-center">UPLOAD BERITA</h3>
-              </div><hr>
-              <form class="user" method="POST" action="">
-                <div class="form-group row">
-
-                  <div class="col-sm-11">
-                    
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-sm-1">
-                    <svg style="width:30px;height:30px" viewBox="0 0 24 24">
-                      <path fill="#989898" d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z" />
-                    </svg>
-                  </div>
-                  <div class="col-sm-5">
-                    <input type="text" class="datepicker form-control form-control-user" autocomplete="off" id="tanggal" name="tanggal" placeholder="Tanggal">
-                  </div>
-                  <div class="col-sm-1">
-                    <svg style="width:30px;height:30px" viewBox="0 0 24 24">
-                      <path fill="#989898" d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z" />
-                    </svg>
-                  </div>
-                  <div class="col-sm-5">
-                     <input type="text" class="form-control form-control-user" autocomplete="off" id="jam" name="jam" placeholder="Jam">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-sm-1">
-                   <svg style="width:30px;height:30px" viewBox="0 0 24 24">
-                    <path fill="#989898" d="M15,12H16.5V16.25L19.36,17.94L18.61,19.16L15,17V12M23,16A7,7 0 0,1 16,23C13,23 10.4,21.08 9.42,18.4L8,17.9L2.66,19.97L2.5,20A0.5,0.5 0 0,1 2,19.5V4.38C2,4.15 2.15,3.97 2.36,3.9L8,2L14,4.1L19.34,2H19.5A0.5,0.5 0 0,1 20,2.5V10.25C21.81,11.5 23,13.62 23,16M9,16C9,12.83 11.11,10.15 14,9.29V6.11L8,4V15.89L9,16.24C9,16.16 9,16.08 9,16M16,11A5,5 0 0,0 11,16A5,5 0 0,0 16,21A5,5 0 0,0 21,16A5,5 0 0,0 16,11Z" />
-                  </svg>
-                </div>
-                <div class="text-center section_title">
-                	<select class="form-control">
-
-                      
-                        <option value=""></option>
-                    
-                      
-                    </select>
-                  <h3>
-                    Katergori Berita
-                  </h3>
-                </div><hr>
-                    <br>
-                    <br>
-                    <br>
-                    <div class="text-center">
-                      <input class="btn btn-primary btn-lg btn-block"  type="submit" name="button" value="Submit">
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+<br>
+<div class="container">		
+	<h1 class="text-center">Upload Berita</h1> 
+	<br>
+	<br>
+	<br>
+	<br
+	<form>
+		<div class="form-group row">
+			<label for="judulberita" class="col-sm-2 col-form-label">Judul Berita</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="judulberita" placeholder="">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="subjudulberita" class="col-sm-2 col-form-label">Sub Judul Berita</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="subjudulberita" placeholder="">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="judulberita" class="col-sm-2 col-form-label">Tanggal</label>
+			<div class="col-sm-3">
+				<input type="text" class="form-control" id="judulberita" placeholder="">
+			</div>
+			<label for="judulberita" class="col-form-label">Jam</label>
+			<div class="col-sm-3">
+				<input type="text" class="form-control" id="judulberita" placeholder="">
+			</div>
+		</div>
+		<fieldset class="form-group">
+    <div class="row">
+      <legend class="col-form-label col-sm-2 pt-0">Kategori Berita</legend>
+      <div class="col-sm-10">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="gridRadios" id="dts" value="option1" checked>
+          <label class="form-check-label" for="dts">
+            DTS
+          </label>
         </div>
-      </form>
-  </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="gridRadios" id="nondts" value="option2">
+          <label class="form-check-label" for="nondts">
+            Non DTS
+          </label>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+  <div class="form-group row">
+			<label for="ringkasan" class="col-sm-2 col-form-label">Ringkasan Berita</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="ringkasan" placeholder="">
+			</div>
+		</div>
+  <div class="form-group">
+    <label for="isiberita">Isi Berita</label>
+    <textarea class="form-control" id="isiberita" rows="7"></textarea>
+  </div>		
+		<button type="submit" class="btn btn-primary">Simpan</button>
+	</form>
 </div>
-</div>
+
 <br>
 <br>
 <br>
