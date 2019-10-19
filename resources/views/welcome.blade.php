c5f196a4664370c1b803399b8c8e7d6749facdea
Sri Rahayu <07,srirahayu@gmail.com>
2019-10-19 23:49:45 +0700

    dah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                                 <li><a style="color:black;" href="{{url('/home')}}">User</a></li>
                             </ul></li>
                                 <li class="nav-item"><a style="color: white;" class="nav-link" href="{{route('register')}}">Register</a></li>
-                            </ul>
+                            </ul></ul>
                         </div>
                 </div>
           </nav>
@@ -96,8 +96,14 @@
   <!-- Modal body -->
   <div class="modal-body">
     <img src="{{asset('img/dts.png')}}" alt="">
-    <div class="container text-center">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.035759999712!2d106.95690725069078!3d-6.259019995447367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698d00ab3824a3%3A0x556821c39752eb5e!2sGunadarma%20University%20J6!5e0!3m2!1sen!2sid!4v1571494326208!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    <div class="container">
+      <div class="">
+        tulis disini mi
+      </div>
+      <div class="text-rigth">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.035759999712!2d106.95690725069078!3d-6.259019995447367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698d00ab3824a3%3A0x556821c39752eb5e!2sGunadarma%20University%20J6!5e0!3m2!1sen!2sid!4v1571494326208!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      </div>
+
 
     </div>
   </div>
