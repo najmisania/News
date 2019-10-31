7b027d62a5ddccd6dbaad881551ae436e495d23c
faldiega <faldiega@gmail.com>
2019-10-31 14:33:12 +0700

    perbaikan pada view history

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -1,54 +1,51 @@
 @include('navbar.head')
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel header-top">
-        <div class="container">
-            <a href="/">
-                <img class="img-fluid" src="{{asset('img/logo-2.png')}}" alt=""></a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div class="col-lg-10 align-items-center justify-content-between col-md-6 col-sm-6 col-6 header-top-left no-padding">
-                        </div>
-                        <div class="col-lg-9 align-items-center justify-content-between col-md-6 col-sm-6 col-6 header-top-right">
-                            <ul class="nav-menu-container navbar-nav ml-auto">
-                            <ul class="nav-menu">
-                                
-                                    <li><a style="color:white;" href="\upload">Upload News</a></li>
-                                </ul></ul>
-                        </div>
-                    </div>
-                </div>
-                </nav>
-<br>
-<br>
-<br>
-<br>
-<div class="col-lg-12">
+  <div class="container">
+    <a href="/"><img class="img-fluid" src="{{asset('img/logo-2.png')}}" alt=""></a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="col-lg-10 align-items-center justify-content-between col-md-6 col-sm-6 col-6 header-top-left no-padding">
+      </div>
+      <div class="col-lg-9 align-items-center justify-content-between col-md-6 col-sm-6 col-6 header-top-right">
+        <ul class="nav-menu-container navbar-nav ml-auto">
+          <ul class="nav-menu">
+            <li><a style="color:white;" href="\upload">Upload News</a></li>
+          </ul>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="col-lg-12"> 
   <div class="card">
-    
     <div class="card-body">
       <div class="table-responsive">
         <div class="row">
           <div class="col-lg-12">
             <div class="p-2">
+
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4"></h1>
               </div>
-              <h4 colspan="3" class="text-center">HISTORY BERITA</h4>
               @include('layouts.alert')
-              
+              <br><br>
+              <h4 colspan="3" class="text-center">HISTORY BERITA</h4>
               <br><br>
               <form class="user" method="post" action="">
-                <table  class="table">
+                <table class="table">
                   <tr>
                     <th width="5%" class=" text-center">NO</th>
                     <th width="15%" class=" text-center">Judul Berita</th>
 
                     <th width="10"class=" text-center">Kategori Berita</th>
 
-                    
+
                     <th width="10"class=" text-center">Tanggal</th>
 
                     <th width="10"class=" text-center">Status</th>
                     <th width="1" class="text-center">Aksi</th>
                   </tr>
+
                   @php
                   $no = 1;
                   @endphp
@@ -58,15 +55,14 @@
                     <td>{{ $brt->judul }}</td>
 
                     <td class=" text-center">{{ $brt->kategori }}</td>
-                    
+
                     <th class=" text-center">{{ $brt->created_at}}</th>
                     <td></td>
 
-
                     <td class=" text-center">
                       <a href="/history/lihat/{{$brt->id}}" class=" btn btn-primary btn-sm">Lihat Berita</a>
-                      
-                      <a href="/history/edit/{{ $brt->id }}" class=" btn btn-warning btn-sm"><i class="fa fa-edit" aria-hidden="true"></a></i>
+
+                      <a href="/history/edit/{{ $brt->id }}" class=" btn btn-warning btn-sm"><i class="fa fa-edit" aria-hidden="true"></i></a>
                       <a href="/history/hapus/{{ $brt->id }}" class=" btn btn-danger btn-sm"><i class="fa fa-trash" aria-hidden="true"></i></a>
                     </td>
                   </tr>
@@ -84,11 +80,5 @@
     </div>
   </div>
 </div>
-</div>
-</div>
-</div>
-</div>
-<br>
-<br>
-<br>
+
 @include('navbar.footer')
